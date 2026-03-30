8e3747dd53c59a6eb85f8c6cd447721bcd5a2a1f
Bryan José <bryanfreitasmg@gmail.com>
2026-03-30 18:35:33 -0300

    Logo na loading screen

--- src/Verasium.Web/src/components/LoadingScreen.jsx
+++ src/Verasium.Web/src/components/LoadingScreen.jsx
@@ -1,3 +1,5 @@
+import logo from "../assets/logo.png";
+
 export default function LoadingScreen() {
   return (
     <div className="screen-page">
@@ -5,12 +7,7 @@ export default function LoadingScreen() {
         <div className="loading-wrap">
           {/* Logo */}
           <div className="loading-logo">
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="11" cy="11" r="7" stroke="#4A5FD4" strokeWidth="1.8" />
-              <line x1="16" y1="16" x2="21" y2="21" stroke="#4A5FD4" strokeWidth="1.8" strokeLinecap="round" />
-              <circle cx="11" cy="11" r="2.5" fill="#4A5FD4" fillOpacity="0.35" />
-            </svg>
-            Verasium
+            <img src={logo} alt="Verasium" className="loading-logo-img" draggable={false} />
           </div>
 
           {/* Scan card with magnifier */}
